759460a3d1c2dbcdc3d5e1ed2ff53adb7c3af2dd
jwinder <joseph.winder@t8webware.com>
2012-08-25 15:48:27 -0500

    adding json validate elisp file

---
+++ json-validate.el
@@ -0,0 +1,28 @@
+;;; json-validate.el
+;;
+;; Helpers for sending jsons to a validator.
+;; Requires: https://github.com/trentm/json
+
+(defun json-validate-buffer ()
+  "Validates the entire buffer for json."
+  (interactive)
+  (json-validate (buffer-substring (point-min) (point-max))))
+
+(defun json-validate-region ()
+  "Validates the entire buffer for json."
+  (interactive)
+  (json-validate (buffer-substring (mark) (point))))
+
+(defun json-validate-string (json)
+  "Validates a provided string of json."
+  (interactive "sJson to validate: ")
+  (json-validate json))
+
+(defun json-validate (json)
+  (setq response (shell-command-to-string (format "echo '%s' | json --validate" json)))
+  (if (= (length response) 0)
+      (message "No errors found.")
+    (message response)))
+
+(provide 'json-validate)
+;;; json-validate.el ends here
