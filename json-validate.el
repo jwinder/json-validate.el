0631c3f8000c5cd1c9787e2fdd7970f1a61d7978
jwinder <joseph.winder@t8webware.com>
2012-08-26 14:08:27 -0500

    Functions for reformatting buffers and regions.

--- json-validate.el
+++ json-validate.el
@@ -4,44 +4,61 @@
 ;; Requires: https://github.com/trentm/json
 
 (defcustom json-validate-display-buffer-name "*Json*"
-  "Name of json response buffer")
+  "Name of json response buffer.")
 
 (defun json-validate-buffer ()
-  "Validates the entire buffer for json."
+  "Validates the buffer of json."
   (interactive)
-  (json-validate (buffer-substring (point-min) (point-max))))
+  (json-validate-for-errors (buffer-substring (point-min) (point-max))))
 
 (defun json-validate-region ()
-  "Validates the entire buffer for json."
+  "Validates a region of json."
   (interactive)
-  (json-validate (buffer-substring (mark) (point))))
+  (json-validate-for-errors (buffer-substring (mark) (point))))
 
 (defun json-validate-string (json)
   "Validates a provided string of json."
   (interactive "sJson to validate: ")
-  (json-validate json))
+  (json-validate-for-errors json))
 
-(defun json-display-string (json)
-  "Displays a json as shell command output with formatting."
+(defun json-format-buffer ()
+  "Formats a buffer of json."
+  (interactive)
+  (setq json (buffer-substring (point-min) (point-max)))
+  (setq response (json-reformat json))
+  (json-insert-in-current-buffer response (point-min) (point-max)))
+
+(defun json-format-region ()
+  "Formats a region of json."
+  (interactive)
+  (setq json (buffer-substring (mark) (point)))
+  (setq response (json-reformat json))
+  (json-insert-in-current-buffer response (mark) (point)))
+
+(defun json-format-string (json)
+  "Formats and displays a json string."
   (interactive "sJson to display (formatted): ")
-  (json-display json))
+  (setq response (json-reformat json))
+  (json-insert-in-json-buffer response))
 
-(defun json-validate (json)
+(defun json-validate-for-errors (json)
   (setq response (shell-command-to-string (format "echo '%s' | json --validate" json)))
   (if (= (length response) 0)
       (message "No errors found.")
     (message response)))
 
-(defun json-display (json)
-  (setq response (shell-command-to-string (format "echo '%s' | json" json)))
-  (json-display-in-json-buffer response))
+(defun json-reformat (json)
+  (shell-command-to-string (format ("echo '%s' | json" json))))
 
-(defun json-display-in-json-buffer (json)
-  (json-get-buffer)
-  (delete-region (point-min) (point-max))
+(defun json-insert-in-current-buffer (json start end)
+  (delete-region start end)
   (insert json))
 
-(defun json-get-buffer ()
+(defun json-insert-in-json-buffer (json)
+  (json-get-json-buffer)
+  (json-insert-in-current-buffer json))
+
+(defun json-get-json-buffer ()
   (set-buffer (get-buffer-create json-validate-display-buffer-name))
   (json-switch-to-js-mode (current-buffer))
   (switch-to-buffer-other-window (current-buffer)))
