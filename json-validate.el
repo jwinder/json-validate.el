b84f0b7200e9bded2862ba35e9282033231aa79b
jwinder <joseph.winder@t8webware.com>
2012-08-26 12:57:01 -0500

    Adding json display defun.

--- json-validate.el
+++ json-validate.el
@@ -18,11 +18,20 @@
   (interactive "sJson to validate: ")
   (json-validate json))
 
+(defun json-display-string (json)
+  "Displays a json as shell command output with formatting."
+  (interactive "sJson to display (formatted): ")
+  (json-display json))
+
 (defun json-validate (json)
   (setq response (shell-command-to-string (format "echo '%s' | json --validate" json)))
   (if (= (length response) 0)
       (message "No errors found.")
     (message response)))
+
+(defun json-display (json)
+  (interactive)
+  (shell-command format ("echo '%s' | json %s" json)))
 
 (provide 'json-validate)
 ;;; json-validate.el ends here
